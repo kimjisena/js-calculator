74765f76cb3a0292adeae1215f7b19cdc49e1545
jisena.btc <pjohmaths@gmail.com>
2022-07-26 16:16:42 +0300

    implement calculator layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import Input from "./components/Input";
 
 function App() {
   return (
-    <div className={`app`}>
-      <Display />
-      <Input />
+    <div className={`flex flex-col justify-center items-center w-full h-[100vh]`}>
+      <div className={`app flex flex-col justify-between items-center w-96 h-[80%] bg-input-wrapper`}>
+        <Display />
+        <Input />
+      </div>
     </div>
   );
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,31 +3,44 @@ import React from 'react';
 function Input () {
     return (
         <>
-            <div id='stdin'>
-                <div id='equals'>=</div>
+            <div id='stdin' className={`flex flex-col justify-evenly h-[60%] w-[90%] bg-input-wrapper`}>
+                {/* first row: 7, 8, 9, DEL, AC*/}
+                <div id='first-row' className={`flex justify-evenly`}>
+                    <div id='seven'>7</div>
+                    <div id='eight'>8</div>
+                    <div id='nine'>9</div>
+                    <div id='delete'>DEL</div>
+                    <div id='clear'>AC</div>
+                </div>
 
-                <div id='zero'>0</div>
-                <div id='one'>1</div>
-                <div id='two'>2</div>
-                <div id='three'>3</div>
-                <div id='four'>4</div>
-                <div id='five'>5</div>
-                <div id='six'>6</div>
-                <div id='seven'>7</div>
-                <div id='eight'>8</div>
-                <div id='nine'>9</div>
 
-                <div id='add'>+</div>
-                <div id='subtract'>-</div>
-                <div id='multiply'>0</div>
-                <div id='divide'>/</div>
+                {/* second row: 4, 5, 6, x, / */}
+                <div id='second-row' className={`flex justify-evenly`}>
+                    <div id='four'>4</div>
+                    <div id='five'>5</div>
+                    <div id='six'>6</div>
+                    <div id='multiply'>x</div>
+                    <div id='divide'>/</div>  
+                </div>
 
-                <div id='decimal'>.</div>
+                {/* third row: 1, 2, 3, +, - */}
+                <div id='third-row' className={`flex justify-evenly`}>
+                    <div id='one'>1</div>
+                    <div id='two'>2</div>
+                    <div id='three'>3</div>
+                    <div id='add'>+</div>
+                    <div id='subtract'>-</div>
+                </div>
 
-                <div id='clear'>AC</div>
-                <div id='delete'>DEL</div>
-                <div id='exponent'>EXP</div>
-                <div id='answer'>Ans</div>
+
+                {/* fourth row: 0, ., EXP, Ans, = */}
+                <div id='fourth-row' className={`flex justify-evenly`}>
+                    <div id='zero'>0</div>
+                    <div id='decimal'>.</div>
+                    <div id='exponent'>EXP</div>
+                    <div id='answer'>Ans</div>
+                    <div id='equals'>=</div>
+                </div>
             </div>
         </>
     );
